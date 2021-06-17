3b293a97fafc2f49e0482f8b9e5bd9163c904d2d
Nathalia Laudano <nathalialaudano@crazytechlabs.com.br>
2021-06-17 15:04:29 -0300

    aulas 1-4

---
+++ ByteBank4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBank4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ContaCorrente conta = new ContaCorrente(0, 0);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.ParamName);
+            }
+        }
+    }
+}
